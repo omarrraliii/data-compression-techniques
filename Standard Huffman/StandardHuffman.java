b20929deb688bf068ce9ffc7c402198387baaf0a
farahhsulaimann03 <farahmagedg14@gmail.com>
2023-11-28 05:31:55 +0200

    edit compression

--- Standard Huffman/StandardHuffman.java
+++ Standard Huffman/StandardHuffman.java
@@ -1,6 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import javax.swing.*;
 
@@ -51,14 +52,15 @@ public class StandardHuffman {
             }
         });
     }
-    private void decompressAction(){
+
+    private void decompressAction() {
         // Create a file chooser dialog to allow the user to select a file and store the user's selection
         JFileChooser fileChooser = new JFileChooser();
         int result = fileChooser.showOpenDialog(frame);
         // Check if the user selected a file (clicked "Open" and did not close the window)
         if (result == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile(); // Get the selected file to be decompressed
-            decompress (selectedFile);
+            decompress(selectedFile);
         }
     }
 
@@ -73,46 +75,46 @@ public class StandardHuffman {
             compress(selectedFile);
         }
     }
-    private void decompress(File file){
-        try  {
-            Scanner scanner = new Scanner(file);
+
+    private void decompress(File file) {
+        try {
+            Scanner scanner = new Scanner(file, StandardCharsets.ISO_8859_1.name());
             StringBuilder compressedBytes = new StringBuilder();
             compressedBytes.append(scanner.nextLine());
-            String compressedBytesString=compressedBytes.toString();
-            StringBuilder binaryStringBuilder= new StringBuilder();
+            String compressedBytesString = compressedBytes.toString();
+            StringBuilder binaryStringBuilder = new StringBuilder();
             String huffmanCodes;
-            boolean isFirstOcurrence=true;
-            for (int i=0;i<compressedBytesString.length();i++){
-                
-                if ((compressedBytesString.charAt(i)=='%') && (isFirstOcurrence)){
-                    isFirstOcurrence=false;
-                    continue;
-                }
-                if(compressedBytesString.charAt(i)=='%')
-                    break;
-                binaryStringBuilder.append(Integer.toBinaryString((int)compressedBytesString.charAt(i)));
+            for (int i = 0; i < compressedBytesString.length(); i++) {
+                binaryStringBuilder.append(String.format("%8s", Integer.toBinaryString((int) compressedBytesString.charAt(i) & 0xFF)).replace(' ', '0'));
             }
-            huffmanCodes=binaryStringBuilder.toString();
-            Map <String,String> huffmanTable=new HashMap<>();
-            while (scanner.hasNext()){
-                huffmanTable.put(scanner.next(),scanner.next());
+            huffmanCodes = binaryStringBuilder.toString();
+            Map<String, String> huffmanTable = new HashMap<>();
+            while (scanner.hasNext()) {
+                huffmanTable.put(scanner.next(), scanner.next());
             }
+//            while (scanner.hasNext()) {
+//                String bit = scanner.next();
+//                if (bit.equals("%")) {
+//                    break;
+//                }
+//                huffmanTable.put(scanner.next(), scanner.next());
+//            }
             scanner.close();
-            String searchString="";
-            String originalData="";
-            for(int i=0;i<huffmanCodes.length();i++){
-                searchString+=huffmanCodes.charAt(i);
-                for(Map.Entry<String,String> entry:huffmanTable.entrySet()){
-                    if (searchString.equals(entry.getValue())){
-                        originalData+=entry.getKey();
-                        searchString="";
+            String searchString = "";
+            String originalData = "";
+            for (int i = 0; i < huffmanCodes.length(); i++) {
+                searchString += huffmanCodes.charAt(i);
+                for (Map.Entry<String, String> entry : huffmanTable.entrySet()) {
+                    if (searchString.equals(entry.getValue())) {
+                        originalData += entry.getKey();
+                        searchString = "";
                     }
                 }
             }
-            try  {
-                File decompressedFile=new File("decompressed.txt");
+            try {
+                File decompressedFile = new File("decompressed.txt");
                 decompressedFile.createNewFile();
-                FileWriter fileWriter =new FileWriter("decompressed.txt");
+                FileWriter fileWriter = new FileWriter("decompressed.txt");
                 fileWriter.write(originalData);
                 fileWriter.close();
                 textArea.setText("File is decompressed successfully in decompressed.txt");
@@ -123,6 +125,7 @@ public class StandardHuffman {
             System.out.println("Please make sure the file exists.");
         }
     }
+
     private void compress(File file) {
         try {
             // Get the original text from the file
@@ -164,9 +167,9 @@ public class StandardHuffman {
                 compressedString.append(codes.get(c));
             }
 
-            int padding = (8 - compressedString.length() % 8) % 8;
-            compressedString.append("%" + "0".repeat(padding));
-            System.out.println("Compressed Data (Huffman Codes): " + compressedString.toString());
+//            int padding = (8 - compressedString.length() % 8) % 8;
+//            compressedString.append("%" + "0".repeat(padding));
+//            System.out.println("Compressed Data (Huffman Codes): " + compressedString.toString());
 
             // Convert the compressed string to bytes
             byte[] compressedBytes = new byte[(compressedString.length() + 7) / 8];
@@ -177,7 +180,6 @@ public class StandardHuffman {
             }
 
             try (FileOutputStream compressedFile = new FileOutputStream("compressed.bin")) {
-                compressedFile.write("%".getBytes());
                 compressedFile.write(compressedBytes);
                 for (Map.Entry<Character, String> code : codes.entrySet()) {
                     compressedFile.write(("\n" + code.getKey() + " " + code.getValue()).getBytes());
