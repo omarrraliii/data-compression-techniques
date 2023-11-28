cbd8490ecc91617be3cb10e8851e9962b7df8bff
omarrraliii <omaralitaha98@gmail.com>
2023-11-28 06:31:47 +0200

    Standard Huffman Done

--- Standard Huffman/StandardHuffman.java
+++ Standard Huffman/StandardHuffman.java
@@ -80,23 +80,69 @@ public class StandardHuffman {
             compressedBytes.append(scanner.nextLine());
             String compressedBytesString=compressedBytes.toString();
             StringBuilder binaryStringBuilder= new StringBuilder();
-            String huffmanCodes;
-            boolean isFirstOcurrence=true;
-            for (int i=0;i<compressedBytesString.length();i++){
-                
-                if ((compressedBytesString.charAt(i)=='%') && (isFirstOcurrence)){
-                    isFirstOcurrence=false;
-                    continue;
-                }
-                if(compressedBytesString.charAt(i)=='%')
-                    break;
-                binaryStringBuilder.append(Integer.toBinaryString((int)compressedBytesString.charAt(i)));
-            }
-            huffmanCodes=binaryStringBuilder.toString();
+            String huffmanCodes="";
+            // boolean isFirstOcurrence=true;
             Map <String,String> huffmanTable=new HashMap<>();
             while (scanner.hasNext()){
                 huffmanTable.put(scanner.next(),scanner.next());
             }
+            // for(Map.Entry<String,String> entry:huffmanTable.entrySet()){
+            //     if(entry.getValue().contains("1")){
+            //         continue;
+            //     }
+            //     huffmanCodes=entry.getKey();
+            // }
+            
+            for (int i=0;i<compressedBytesString.length();i++){
+                
+                // if ((compressedBytesString.charAt(i)=='%') && (isFirstOcurrence)){
+                //     isFirstOcurrence=false;
+                //     continue;
+                // }
+                // if(compressedBytesString.charAt(i)=='%')
+                //     break;
+                // int compressedBytesStringValue= (int)compressedBytesString.charAt(i);
+                // long unsignedcompressedBytesStringValue=Integer.toUnsignedLong(compressedBytesStringValue);
+                if(i==compressedBytesString.length()-1){
+                    int index=0;
+                    String lastByteModifier=Integer.toBinaryString((int)compressedBytesString.charAt(i));
+                    for(int j=0;j<lastByteModifier.length();j++){
+                        if(lastByteModifier.charAt(j)=='1'){
+                            index=j;
+                        }
+                    }
+                    String finalOriginalValue="";
+                    for(int j=0;j<=index;j++){
+                        finalOriginalValue+=lastByteModifier.charAt(j);
+                    }
+                    binaryStringBuilder.append(finalOriginalValue);
+                    break;
+                }
+                if(((int)compressedBytesString.charAt(i)>=64)&& ((int)compressedBytesString.charAt(i)<128)){
+                    binaryStringBuilder.append("0");
+                }
+                else if((int)compressedBytesString.charAt(i)>=32){
+                    binaryStringBuilder.append("00");
+                }
+                else if((int)compressedBytesString.charAt(i)>=16){
+                    binaryStringBuilder.append("000");
+                }
+                else if((int)compressedBytesString.charAt(i)>=8){
+                    binaryStringBuilder.append("0000");
+                }
+                else if((int)compressedBytesString.charAt(i)>=4){
+                    binaryStringBuilder.append("00000");
+                }
+                else if((int)compressedBytesString.charAt(i)>=2){
+                    binaryStringBuilder.append("000000");
+                }
+                else if((int)compressedBytesString.charAt(i)>=1){
+                    binaryStringBuilder.append("0000000");
+                }
+                binaryStringBuilder.append(Integer.toBinaryString((int)compressedBytesString.charAt(i)));
+            }
+            huffmanCodes=binaryStringBuilder.toString();
+            
             scanner.close();
             String searchString="";
             String originalData="";
@@ -136,27 +182,30 @@ public class StandardHuffman {
             // Calculate character probabilities of the original text
             Map<Character, Integer> probabilities = new HashMap<>();
             for (char c : originalData.toString().toCharArray()) {
-                probabilities.put(c, probabilities.getOrDefault(c, 0) + 1);
+                 probabilities.put(c, probabilities.getOrDefault(c, 0) + 1); // Ensure that the default frequency is 0 if a character is encountered for the first time
             }
 
-            // Create a priority queue
+            // Create a priority queue (to handle the ordering of characters based on their probabilities)
             PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparingInt(node -> node.frequency));
             for (Map.Entry<Character, Integer> entry : probabilities.entrySet()) {
-                Node newNode = new Node(entry.getKey(), entry.getValue());
+                Node newNode = new Node(entry.getKey(), entry.getValue()); // Create a new Node for the current character and its probability
                 queue.add(newNode);
             }
 
-            while (queue.size() > 1) {
+            while (queue.size() > 1) { // Continue until there is only one node with one probability
+                // The two nodes with the lowest probabilities are removed
                 Node left = queue.poll();
                 Node right = queue.poll();
+
+                // Add a new combined node with a probability equal to the sum of the probabilities of the two nodes removed
                 Node combined = new Node('\0', left.frequency + right.frequency);
-                combined.left = left;
+                combined.left = left; // The left and right children of the new node are set to the nodes whose probabilities were added
                 combined.right = right;
                 queue.add(combined);
             }
 
-            Map<Character, String> codes = new HashMap<>();
-            buildCodes(queue.peek(), "", codes);
+            Map<Character, String> codes = new HashMap<>(); // To store the codes for each character
+            buildCodes(queue.peek(), "", codes); // Pass the root to assign the other added characters their own code
 
             // Convert each character of the original text to its code
             StringBuilder compressedString = new StringBuilder();
@@ -164,20 +213,16 @@ public class StandardHuffman {
                 compressedString.append(codes.get(c));
             }
 
-            int padding = (8 - compressedString.length() % 8) % 8;
-            compressedString.append("%" + "0".repeat(padding));
-            System.out.println("Compressed Data (Huffman Codes): " + compressedString.toString());
-
             // Convert the compressed string to bytes
             byte[] compressedBytes = new byte[(compressedString.length() + 7) / 8];
             for (int i = 0; i < compressedString.length(); i++) {
-                if (compressedString.charAt(i) == '1') {
+                if (compressedString.toString().charAt(i) == '1') {
+                    // Set the corresponding bit in the byte
                     compressedBytes[i / 8] |= (byte) (1 << (7 - (i % 8)));
                 }
             }
 
             try (FileOutputStream compressedFile = new FileOutputStream("compressed.bin")) {
-                compressedFile.write("%".getBytes());
                 compressedFile.write(compressedBytes);
                 for (Map.Entry<Character, String> code : codes.entrySet()) {
                     compressedFile.write(("\n" + code.getKey() + " " + code.getValue()).getBytes());
@@ -190,25 +235,21 @@ public class StandardHuffman {
             System.out.println("Please make sure the file exists.");
         }
     }
-
     private void buildCodes(Node root, String code, Map<Character, String> huffmanCodes) {
         // Check that the node is not null (have a character with its probability)
         if (root != null) {
             if (root.character != '\0') { // If the node is not an internal node and has a character with its probability
                 huffmanCodes.put(root.character, code);
             }
-
             // Assign 0 to one of its children and 1 to the other until all nodes (characters) are assigned a code
             buildCodes(root.left, code + "0", huffmanCodes);
             buildCodes(root.right, code + "1", huffmanCodes);
         }
     }
-
     class Node {
         char character;
         int frequency;
         Node left, right;
-
         public Node(char character, int frequency) {
             this.character = character;
             this.frequency = frequency;
